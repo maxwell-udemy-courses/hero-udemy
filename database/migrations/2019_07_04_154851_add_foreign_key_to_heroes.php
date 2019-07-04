58b1a7de147456d7fe8bd90cccaf0db606248553
Maximiliano Burgos <maxib2007@gmail.com>
2019-07-04 13:09:21 -0300

    se genero campo clave

---
+++ database/migrations/2019_07_04_154851_add_foreign_key_to_heroes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyToHeroes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('heroes', function (Blueprint $table) {
+            $table->biginteger('level_id')->unsigned();
+
+            $table->foreign('level_id')->references('id')->on('levels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('heroes', function (Blueprint $table) {
+            //
+        });
+    }
+}
