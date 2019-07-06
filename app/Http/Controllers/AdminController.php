d728adcd383f6596e740079313434bac14a889c5
Maximiliano Burgos <maxib2007@gmail.com>
2019-07-06 19:02:45 -0300

    se armo admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Hero;
+use App\Item;
+use App\Enemy;
 
 class AdminController extends Controller
 {
     public function index(){
-        return view('admin.index');
+        $heroCounter = Hero::count();
+        $itemCounter = Item::count();
+        $enemyCounter = Enemy::count();
+
+        $report = [
+            ['name' => "Heroes", 'counter' => $heroCounter, 'route' => 'heroes.index', 'class' => 'btn-primary'],
+            ['name' => "Items", 'counter' => $itemCounter, 'route' => 'item.index', 'class' => 'btn-warning'],
+            ['name' => "Enemigos", 'counter' => $enemyCounter, 'route' => 'enemy.index', 'class' => 'btn-danger']
+        ];
+
+        return view('admin.index', ['report' => $report]);
     }
 }
